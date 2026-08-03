b9bd2a43130259adcf0f94fe525b342f302d2865
ByteBard <cwchen123@gmail.com>
2026-08-03 17:50:34 +0800

    Refactor the codebase

---
+++ cli.ml
@@ -0,0 +1,53 @@
+let version = "0.1.0"
+
+#include "flag.ml"
+#include "dict.ml"
+#include "lexer.ml"
+
+type command =
+  | Version
+  | Help
+  | Sentence of string
+
+let arg_parse argv =
+  match Array.to_list argv with
+  | [_] -> Error "No sentence"
+  | [_; "-v"] | [_; "--version"] -> Ok Version
+  | [_; "-h"] | [_; "--help"] -> Ok Help
+  | [_; arg] when String.starts_with ~prefix:"-" arg ->
+    Error (Printf.sprintf "Unknown option: %s" arg)
+  | [_; sentence] -> Ok (Sentence sentence)
+  | _ -> Error "Too many arguments"
+
+type stream =
+  | Stdout
+  | Stderr
+
+let help_info stream =
+  let template = Printf.sprintf "Usage: %s [option] <sentence>" Sys.argv.(0) in
+  match stream with
+  | Stdout -> print_endline template
+  | Stderr -> prerr_endline template
+
+let _ =
+  let cmd = arg_parse Sys.argv in
+  match cmd with
+  | Ok Version ->
+    Printf.printf "%s\n" version
+  | Ok Help ->
+    help_info Stdout
+  | Ok (Sentence sentence) ->
+    Printf.printf "# %s\n" sentence;
+    lex rules sentence
+    |> List.iter (fun x -> print_endline (print_token x));
+    #ifdef hakka
+    Printf.printf "(%s, %s, \"%s\", \"%s\")\n" "EOS" (string_of_pos End) "all dialect" "end of sentence"
+    #else
+    #ifdef taigi
+    Printf.printf "(%s, %s, %s, \"%s\")\n" "EOS" "EOS" (string_of_pos End) "end of sentence"
+    #endif
+    #endif
+  | Error err ->
+    prerr_endline err;
+    help_info Stderr;
+    exit 1
--- lexer.ml
+++ lexer.ml
@@ -1,33 +1,3 @@
-let version = "0.1.0"
-
-#include "flag.ml"
-#include "dict.ml"
-
-type command =
-  | Version
-  | Help
-  | Sentence of string
-
-let arg_parse argv =
-  match Array.to_list argv with
-  | [_] -> Error "No sentence"
-  | [_; "-v"] | [_; "--version"] -> Ok Version
-  | [_; "-h"] | [_; "--help"] -> Ok Help
-  | [_; arg] when String.starts_with ~prefix:"-" arg ->
-    Error (Printf.sprintf "Unknown option: %s" arg)
-  | [_; sentence] -> Ok (Sentence sentence)
-  | _ -> Error "Too many arguments"
-
-type stream =
-  | Stdout
-  | Stderr
-
-let help_info stream =
-  let template = Printf.sprintf "Usage: %s [option] <sentence>" Sys.argv.(0) in
-  match stream with
-  | Stdout -> print_endline template
-  | Stderr -> prerr_endline template
-
 let rec take n lst =
   if n <= 0 then []
   else match lst with
@@ -117,26 +87,3 @@ let lex (rules : rule list) (str : string) : token list =
             loop remaining (text_tok :: acc)
   in
   loop tokens []
-
-let _ =
-  let cmd = arg_parse Sys.argv in
-  match cmd with
-  | Ok Version ->
-    Printf.printf "%s\n" version
-  | Ok Help ->
-    help_info Stdout
-  | Ok (Sentence sentence) ->
-    Printf.printf "# %s\n" sentence;
-    lex rules sentence
-    |> List.iter (fun x -> print_endline (print_token x));
-    #ifdef hakka
-    Printf.printf "(%s, %s, \"%s\", \"%s\")\n" "EOS" (string_of_pos End) "all dialect" "end of sentence"
-    #else
-    #ifdef taigi
-    Printf.printf "(%s, %s, %s, \"%s\")\n" "EOS" "EOS" (string_of_pos End) "end of sentence"
-    #endif
-    #endif
-  | Error err ->
-    prerr_endline err;
-    help_info Stderr;
-    exit 1
